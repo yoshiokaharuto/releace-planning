0a1362afcfeeefa97cbcab8f7ce80f4d0297fd26
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2023-12-12 15:02:25 +0900

    背景を表示させた #2

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -8,67 +8,14 @@ class MainScene extends Phaser.Scene {
     }
     // シーンの事前読み込み処理
     preload() {
-        this.load.image('sky', 'assets/sky.png');
-         this.load.image('back', 'assets/back.png');
-         this.load.image('ground', 'assets/platform.png');
-         this.load.image('alien1', 'assets/alien1.png');
-         this.load.image('alien2', 'assets/alien2.png');
-         this.load.image('alien3', 'assets/alien3.png');
-         this.load.image('platform', 'assets/platform.png');
-         this.load.image('block', 'assets/block.png');
-         this.load.image('bomb', 'assets/bomb.png');
-         this.load.image('star', 'assets/star.png');
-         this.load.image('coin', 'assets/coin.png');
+        this.load.image('sky','assets/background.png')
     }
     // シーン初期化処理
     create() {
-        // 単体画像をシーンに追加(X座標,Y座標,画像名)
-        this.add.image(400, 300, 'sky');
-
-        this.player1 = this.add.image(400,300,'alien1');
-        this.player_direction1 = 1;
-
-        this.player2 = this.add.image(400, 300, 'alien2');
-        this.player_direction2 = 1;
-
-        this.player3 = this.add.image(400, 300, 'alien2');
-        this.player_direction3 = 1;
+        this.add.image(400,300,'sky');
     }
     // 毎フレーム実行される繰り返し処理
     update() {
-         if (this.player1.y >= D_HEIGHT - 100) this.player_direction1 = -1;
-         if (this.player1.y <= 100) this.player_direction1 = 1;
-         if (this.player_direction1 == 1) {
-             this.player1.y += 10;
-         } else {
-             this.player1.y -= 10;
-         }
-
-         if (this.player2.x >= D_WIDTH - 100) this.player_direction2 = -1;
-         if (this.player2.x <= 100) this.player_direction2 = 1;
-         // プレイヤーの移動 
-         if (this.player_direction2 == 1) {
-             this.player2.x += 10;
-         } else {
-             this.player2.x -= 10;
-         }
-
-         if (this.player3.x >= D_WIDTH - 100) this.player_direction3 = -1;
-         if (this.player3.x <= 100) this.player_direction3 = 1;
-         // プレイヤーの移動 
-         if (this.player_direction3 == 1) {
-             this.player3.x += 10;
-         } else {
-             this.player3.x -= 10;
-         }
-         if (this.player3.y >= D_HEIGHT - 100) this.player_direction3 = -1;
-         if (this.player3.y <= 100) this.player_direction3 = 1;
-         // プレイヤーの移動 
-         if (this.player_direction3 == 1) {
-             this.player3.y += 10;
-         } else {
-             this.player3.y -= 10;
-         }
     }
 
 }
