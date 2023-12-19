40b0ad22dac5eac99d214853db90b9450976aedc
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2023-12-19 14:17:34 +0900

    taroとhanakoを配置した

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -9,10 +9,17 @@ class MainScene extends Phaser.Scene {
     // シーンの事前読み込み処理
     preload() {
         this.load.image('sky','assets/background.png')
+        this.load.image('taro','assets/taro.png')
+        this.load.image('hanako','assets/hanako.png')
     }
     // シーン初期化処理
     create() {
         this.add.image(400,300,'sky');
+
+        const taro = this.physics.add.sprite(50,50,'taro');
+        const hanako = this.physics.add.sprite(50,50,'hanako');
+        this.taro = taro 
+        this.hanako = hanako
     }
     // 毎フレーム実行される繰り返し処理
     update() {
